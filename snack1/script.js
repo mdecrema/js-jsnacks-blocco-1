0ffbd8c9a1a7e5b9e6ffa8f033eb4c8d4726a1ae
marco <marcodecrema@libero.it>
2020-08-05 17:18:44 +0200

    snack1 completato

--- snack1/script.js
+++ snack1/script.js
@@ -1,7 +1,9 @@
-var numero = parseInt(prompt("Inserisci un numero"));
+var primoNumero = parseInt(prompt("Inserisci un numero"));
+var secondoNumero = parseInt(prompt("Inserisci un altro numero"));
+var risultato = document.getElementById('risultato');
 
-if (numero % 2 == 0) {
-  console.log(numero);
-} else {
-  console.log(numero + 1);
+if (primoNumero > secondoNumero) {
+  risultato.innerHTML=primoNumero;
+} else if (secondoNumero > primoNumero) {
+  risultato.innerHTML=secondoNumero;
 }
