6dd856c4c10c32de30fc22b5c96cf5afb4f5579e
marco <marcodecrema@libero.it>
2020-08-05 18:49:23 +0200

    aggiunta di 'isNaN'

--- snack1/script.js
+++ snack1/script.js
@@ -5,10 +5,14 @@ var secondoNumero = parseInt(prompt("Inserisci un altro numero"));
 // ID dell'elemento in cui verrà stampato il risultato
 var risultato = document.getElementById('risultato');
 // Metodo
+if (!isNaN(primoNumero) && !isNaN(secondoNumero)) {
 if (primoNumero > secondoNumero) {
   risultato.innerHTML=primoNumero;
 } else if (secondoNumero > primoNumero) {
   risultato.innerHTML=secondoNumero;
 } else {
   risultato.innerHTML="I due numeri sono uguali";
+}
+} else {
+  alert('Errore! Devi inserire un valore numerico');
 }
--- snack3/script.js
+++ snack3/script.js
@@ -3,8 +3,12 @@ var numero = parseInt(prompt("Inserisci un numero"));
 // ID dell'elemento in cui verrà stampato il risultato
 var risultato = document.getElementById('risultato');
 // Metodo
-if (numero % 2 == 0) {
+if (!isNaN(numero)) {
+  if (numero % 2 == 0) {
   risultato.innerHTML=numero;
 } else {
   risultato.innerHTML=numero+1;
+}
+} else {
+  alert('Errore! Devi inserire un valore numerico');
 }
